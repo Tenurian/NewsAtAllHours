83f6b4aed6ce373121544240f7cb13746bb36687
Christian Coreil <ccoreil@student.neumont.edu>
2015-12-12 09:53:28 -0700

    headerLerp.js

--- scripts/headerLerp.js
+++ scripts/headerLerp.js
@@ -1,5 +1,18 @@
 var c = document.getElementById("headerCanvas");
 var ctx = c.getContext("2d");
 var sunImg = document.getElementById("sun");
+var moonImg = document.getElementById("moon");
 
-ctx.drawImage(sunImg, 8, 0, 100, 100);
+
+ctx.drawImage(sunImg, 0, 5, 100, 100);
+
+
+function lerp(x1, x2, scalar) {
+    var vx = (1 - scalar) * x1 + scalar * x2;
+    return Math.floor(vx);
+}
+
+$(document).ready(function () {
+    $(document.getElementById('sun')).hide();
+    $(document.getElementById('moon')).hide();
+});
