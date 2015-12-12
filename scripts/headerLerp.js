8b6e3bf500c62bce4e440e7d885242ef12796661
Christian Coreil <ccoreil@student.neumont.edu>
2015-12-12 11:53:44 -0700

    header lerp working

--- scripts/headerLerp.js
+++ scripts/headerLerp.js
@@ -2,17 +2,52 @@ var c = document.getElementById("headerCanvas");
 var ctx = c.getContext("2d");
 var sunImg = document.getElementById("sun");
 var moonImg = document.getElementById("moon");
+var width = c.width - 100;
+var interval;
 
 
-ctx.drawImage(sunImg, 0, 5, 100, 100);
-
-
-function lerp(x1, x2, scalar) {
-    var vx = (1 - scalar) * x1 + scalar * x2;
-    return Math.floor(vx);
+function moveSun(newNum, hour) {
+    if (hour > 5 || hour < 17) {
+        ctx.clearRect(0, 0, c.width, c.height);
+        ctx.drawImage(sunImg, newNum, 5, 100, 100);
+    } else {
+        ctx.clearRect(0, 0, c.width, c.height);
+        ctx.drawImage(moonImg, newNum, 5, 100, 100);
+    }
 }
+
+interval = setInterval(function () {
+
+    var today = new Date();
+    var hour = today.getHours();
+    var minutes = today.getMinutes();
+    var seconds = today.getSeconds();
+
+    var interpolateHour = (hour / 24) * 100;
+    var interpolateMinutes = (minutes / 60);
+    var interpolateSeconds = (seconds / 3600);
+
+    console.log(interpolateHour + ", " + interpolateMinutes + ", " + interpolateSeconds);
+
+    var num = getTween(0, width, interpolateHour);
+    num += getTween(0, width, interpolateMinutes);
+    num += getTween(0, width, interpolateSeconds);
+    moveSun(num, hour);
+}, 1);
+
+
+function stopInterval() {
+    clearInterval(interval);
+}
+
+function getTween(b, e, i) {
+    return b + ((i / 100) * (e - b));
+}
+
 
 $(document).ready(function () {
     $(document.getElementById('sun')).hide();
     $(document.getElementById('moon')).hide();
 });
+
+//
