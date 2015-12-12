493a4835376ca401d6fb24b027206972ee1b7725
Christian Coreil <ccoreil@student.neumont.edu>
2015-12-12 14:53:39 -0700

    aaaaa

--- scripts/headerLerp.js
+++ scripts/headerLerp.js
@@ -10,7 +10,7 @@ function moveSun(newNum, hour) {
     if (hour > 5 && hour < 17) {
         ctx.clearRect(0, 0, c.width, c.height);
         ctx.drawImage(sunImg, newNum, 5, 100, 100);
-        c.style.backgroundImage = "url('Images/daySky2.jpg')";
+        c.style.backgroundImage = "url('Images/skys.jpg')";
     } else {
         ctx.clearRect(0, 0, c.width, c.height);
         ctx.drawImage(moonImg, newNum, 5, 100, 100);
