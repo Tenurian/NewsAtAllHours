82ead29d0750b23ae2fbdbdcf05e85b9e887c37b
Christian Coreil <ccoreil@student.neumont.edu>
2015-12-14 10:25:22 -0700

    tryed to set up time via script

--- scripts/timeZone.js
+++ scripts/timeZone.js
@@ -1,3 +1,29 @@
- setInterval(function () {
-     document.getElementById("date").innerHTML = new Date();
- }, 1);
+var d = new Date();
+var pacificTimeZone = 0;
+var mountainTimeZone = 0;
+var centralTimeZone = 0;
+var easternTimeZone = 0;
+var alaskanTimeZone = 0;
+var hawaiianTimeZone = 0;
+
+var dateArea = document.getElementById("date1");
+
+setInterval(getTimeZones, 1);
+//d.toLocaleTimeString
+
+function getTimeZones() {
+
+    hawaiianTimeZone = "Hawaii-Aleutian: " + (d.getHours() - 3) + ":" + d.getMinutes() + ":" + d.getSeconds() + "  " + d.getDay() + ", " + d.getMonth() + " " + d.getDay() + ", " + d.getFullYear();
+
+    alaskanTimeZone = "Alaska: " + (d.getHours() - 2) + ": " + d.getMinutes() + d.getSeconds() + "  " + d.getDay() + ", " + d.getMonth() + " " + d.getDay() + ", " + d.getFullYear();
+
+    pacificTimeZone = "Pacific: " + (d.getHours() - 1) + ": " + d.getMinutes() + d.getSeconds() + "  " + d.getDay() + ", " + d.getMonth() + " " + d.getDay() + ", " + d.getFullYear();
+
+    mountainTimeZoneTimeZone = "Mountain: " + d.getHours() + ": " + d.getMinutes() + d.getSeconds() + "  " + d.getDay() + ", " + d.getMonth() + " " + d.getDay() + ", " + d.getFullYear();
+
+    centralTimeZoneTimeZone = "Central: " + (d.getHours() + 1) + ": " + d.getMinutes() + d.getSeconds() + "  " + d.getDay() + ", " + d.getMonth() + " " + d.getDay() + ", " + d.getFullYear();
+
+    easternTimeZone = "Eastern: " + (d.getHours() + 2) + ": " + d.getMinutes() + d.getSeconds() + "  " + d.getDay() + ", " + d.getMonth() + " " + d.getDay() + ", " + d.getFullYear();
+
+    dateArea.innerHTML = hawaiianTimeZone;
+}
